b00ac3405869e76e2556a1f7f570b2ed34adfd74
EnzoAvila05 <enzopaa@hotmail.com>
2024-11-16 17:55:10 -0400

    disciplina class

---
+++ disciplina.py
@@ -0,0 +1,12 @@
+class Disciplina:
+    def __init__(self, nome, carga_horaria, professor=None):
+        self.nome = nome
+        self.carga_horaria = carga_horaria
+        self.professor = professor
+
+    def informacoes_disciplina(self):
+        return {
+            "nome": self.nome,
+            "carga_horaria": self.carga_horaria,
+            "professor": self.professor.nome if self.professor else None
+        }
