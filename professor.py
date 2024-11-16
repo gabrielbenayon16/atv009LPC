ed1b6cc0c277714ac550b6de07e64599cb9d117e
EnzoAvila05 <enzopaa@hotmail.com>
2024-11-16 17:59:50 -0400

    professor class

---
+++ professor.py
@@ -0,0 +1,15 @@
+from disciplina import Disciplina
+
+class Professor:
+    def __init__(self, nome):
+        self.nome = nome
+        self.disciplinas = []
+
+    def adicionar_disciplina(self, disciplina):
+        if len(self.disciplinas) < 5:
+            self.disciplinas.append(disciplina)
+        else:
+            print("O professor já atingiu o limite de 5 disciplinas.")
+
+    def listar_disciplinas(self):
+        return [d.nome for d in self.disciplinas]git
